8926086c293dcf5ea545cc5d81bcf225e15b48ca
Romina Dastoorian <53481910+RominaDst@users.noreply.github.com>
2021-08-21 17:10:54 -0400

    Delete Spatial 3 directory

--- Spatial 3/Stain/Xtra/first_try/stain_maker.m
+++
@@ -1,1 +0,0 @@
-function []=stain_maker(xc,yc,r,c,pic)
--- Spatial 3/local_anis/aniso_maker.m
+++
@@ -1,9 +0,0 @@
-function [field1]= aniso_maker(n,BlkCirc_row);
-m=n;
-lam=real(fft2(BlkCirc_row))/(2*m-1)/(2*n-1);
-lam(lam(:)<0)=0; lam=sqrt(lam);
-% generate field with covariance given by block circulant matrix
-F=fft2(lam.*complex(randn(2*m-1,2*n-1),randn(2*m-1,2*n-1)));
-F=F(1:m,1:n); % extract sub-block with desired covariance
-field1=real(F);
-end
