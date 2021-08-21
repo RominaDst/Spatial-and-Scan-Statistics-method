9cf6ab2e27ecfeecffa78a938a78faf97ec668fc
Romina Dastoorian <53481910+RominaDst@users.noreply.github.com>
2021-08-21 17:09:46 -0400

    Delete Scan 2 directory

--- Scan 2/evalROI.m
+++
@@ -1,7 +0,0 @@
-function [tt]=evalROI(ROIs,I);
-tt=zeros(1580,1);
-
-for p=1:1580
-           tt(p,1)=sum(I(ROIs(:,:,p)));
-end
- 
--- Scan 2/maxim.m
+++
@@ -1,8 +0,0 @@
-function [ rat_max]= maxim(m,X_sum,mus,nums,lratconst);
-rat_max=zeros(m,1);
-for j=1:m
-          muhat=sum(X_sum(:,m-j+1:m),2)./(nums.*j);
-         rats=lratconst.*nums.*j.*(muhat-mus).^2;
-          rat_max(j,1)=(max(rats));
-end
-end
